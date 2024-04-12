430af8ad5364d7cbe5890208eddcabff97ba664e
Yogesh <yogesh.jamdar14@gmail.com>
2024-04-12 14:10:54 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twitter_Sentiment_Analysis_V1
+Twitter Sentiment Analysis is an innovative project that aims to automatically analyze and classify the sentiment of Twitter. The project leverages the power of deep learning techniques to decipher the emotional tone of tweets, helping users gain valuable insights into public opinions, trends, and sentiment fluctuations.
---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pickle
+import time
+import sklearn
+
+
+
+st.title("Twitter Sentiment Analysis")
+g
+model = pickle.load(open('twitter_sentiment_v4.pkl','rb'))
+
+tweet = st.text_input('Enter Tweet')
+
+submit = st.button('Predict Sentiment')
+
+if submit:
+    start = time.time()
+    prediction = model.predict([tweet])
+    end = time.time()
+
+    st.write('Prediction time :',round(end-start,2),' seconds')
+
+    if prediction[0] == 'Positive':
+        st.write("Predicted sentiment is :", prediction[0], "\U0001F603")
+
+    elif prediction[0] == 'Negative':
+        st.write("Predicted sentiment is :", prediction[0], "\U0001F614")
+
+    elif prediction[0] == 'Neutral':
+        st.write("Predicted sentiment is :", prediction[0], "\U0001F610")
+
+    else:
+        st.write("Predicted sentiment is :", prediction[0], "\U0001F937")
+
+    print(prediction[0])
